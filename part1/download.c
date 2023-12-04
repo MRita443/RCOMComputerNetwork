3ac9203de2131eda669ae83e04f83544d62093b3
Eduardo <edurocadas@gmail.com>
2023-12-04 01:52:59 +0000

    Finised, needs debugging

--- part1/download.c
+++ part1/download.c
@@ -2,12 +2,12 @@
  *       tidy up some includes and parameters
  * */
 
-#include <download.h>
+#include "download.h"
 
 int control_socket;
 int data_socket;
 
-int openSocket(char *server_ip, unsigned int server_port, int sockfd)
+void open_socket(char *server_ip, unsigned int server_port, int sockfd)
 {
     struct sockaddr_in server_addr;
 
@@ -32,84 +32,96 @@ int openSocket(char *server_ip, unsigned int server_port, int sockfd)
         perror("connect()");
         exit(-1);
     }
-    return 1;
 }
 
-void getServerResponse(struct serverResponse *response)
+void get_server_response(struct server_response *response)
 {
     memset(&response, 0, sizeof(response));
 
     unsigned int state = ST_READING_CODE;
-    unsigned char currByte;
-    unsigned char buffer[3];
+    unsigned char curr_byte;
+    unsigned char file_content[3];
 
-    unsigned char psvServerInfo[11];
+    unsigned char psv_server_info[11];
     unsigned char ip1, ip2, ip3, ip4;
     unsigned short port1, port2;
 
     unsigned char idx = 0;
+    unsigned short idx_msg = 0;
 
     while (state != ST_END)
     {
-        if (read(control_socket, &currByte, 1) < 0)
+        unsigned short n;
+
+        if (n = read(control_socket, &curr_byte, 1) < 0)
         {
             perror("read() server response");
             exit(-1);
         }
 
-        switch (state)
+        if (n > 0)
         {
-        case ST_READING_CODE:
-            if (currByte == ' ' || currByte == '-')
+            switch (state)
             {
-                sscanf(buffer, "%d", &response->code);
-                if (response->code == SERVER_ENTER_PSV)
-                    state = ST_PARSE_START;
-                else if (currByte == ' ')
-                    state = ST_FLUSH_SINGLE;
+            case ST_READING_CODE:
+                if (curr_byte == ' ' || curr_byte == '-')
+                {
+                    sscanf(file_content, "%hu", &response->code);
+                    if (response->code == SERVER_ENTER_PSV)
+                        state = ST_PARSE_START;
+                    else if (curr_byte == ' ')
+                        state = ST_FLUSH_SINGLE;
+                    else
+                        state = ST_FLUSH_MULT;
+                }
                 else
-                    state = ST_FLUSH_MULT;
+                    file_content[idx++] = curr_byte;
+                break;
+            case ST_FLUSH_SINGLE:
+                response->message[idx_msg++] = curr_byte;
+                if (curr_byte == '\n')
+                    state = ST_END;
+                break;
+            case ST_FLUSH_MULT:
+                response->message[idx_msg++] = curr_byte;
+                if (curr_byte == '\n')
+                {
+                    idx = 0;
+                    state == ST_READING_CODE;
+                }
+                break;
+            case ST_PARSE_START:
+                response->message[idx_msg++] = curr_byte;
+                if (curr_byte == '(')
+                {
+                    idx = 0;
+                    state = ST_PARSE;
+                }
+                break;
+            case ST_PARSE:
+                response->message[idx_msg++] = curr_byte;
+                if (curr_byte == ')')
+                {
+                    sscanf(psv_server_info, "%c,%c,%c,%c,%hu,%hu", &ip1, &ip2, &ip3, &ip4, &port1, &port2);
+                    state = ST_END;
+                }
+                else
+                    psv_server_info[idx++] = curr_byte;
+                break;
+            case ST_END:
+                break;
             }
-            else
-                buffer[idx++] = currByte;
-            break;
-        case ST_FLUSH_SINGLE:
-            if (currByte == '\n')
-                state = ST_END;
-            break;
-        case ST_FLUSH_MULT:
-            if (currByte == '\n')
-            {
-                idx = 0;
-                state == ST_READING_CODE;
-            }
-            break;
-        case ST_PARSE_START:
-            if (currByte == '(')
-            {
-                idx = 0;
-                state = ST_PARSE;
-            }
-            break;
-        case ST_PARSE:
-            if (currByte == ')')
-                sscanf(psvServerInfo, "%d,%d,%d,%d,%d,%d", &ip1, &ip2, &ip3, &ip4, &port1, &port2);
-            else
-                psvServerInfo[idx++] = currByte;
-            break;
-        case ST_END:
-            break;
         }
     }
 
     if (response->code == SERVER_ENTER_PSV)
     {
-        spritnf(response->server_ip, "%d.%d.%d.%d", ip1, ip2, ip3, ip4);
-        response->code = 256 * port1 + port2;
+        sprintf(response->server_ip, "%c.%c.%c.%c", ip1, ip2, ip3, ip4);
+        response->server_port = 256 * port1 + port2;
     }
 }
 
-int getIP(char *hostName, char *ip)
+int get_IP(char *host_name, char *ip)
 {
     struct hostent *h;
 
@@ -128,7 +140,7 @@ int getIP(char *hostName, char *ip)
     #define h_addr h_addr_list[0]	The first address in h_addr_list.
     */
 
-    if ((h = gethostbyname(hostName)) == NULL)
+    if ((h = gethostbyname(host_name)) == NULL)
     {
         herror("gethostbyname()");
         exit(-1);
@@ -142,40 +154,40 @@ int getIP(char *hostName, char *ip)
     return 0;
 }
 
-void parseURl(char *url, struct urlInfo *information)
+void parse_URl(char *url, struct urlInfo *information)
 {
-    regex_t urlUsernamePassword;
-    regex_t urlUsername;
-    regex_t urlNoUser;
+    regex_t url_username_password;
+    regex_t url_username;
+    regex_t url_no_user;
     regex_t ip;
 
-    regcomp(&urlUsernamePassword, REGEX_URL_USERNAME_PASSWORD, REG_NEWLINE);
-    regcomp(&urlUsername, REGEX_URL_USERNAME, REG_NEWLINE);
-    regcomp(&urlNoUser, REGEX_URL_NO_USER, REG_NEWLINE);
-    regcomp(&ip, REGEX_IP, REG_NEWLINE);
+    regcomp(&url_username_password, REGEX_URL_USERNAME_PASSWORD, REG_EXTENDED);
+    regcomp(&url_username, REGEX_URL_USERNAME, REG_EXTENDED);
+    regcomp(&url_no_user, REGEX_URL_NO_USER, REG_EXTENDED);
+    regcomp(&ip, REGEX_IP, REG_EXTENDED);
 
-    if (regexec(&urlUsernamePassword, url, 0, NULL, 0) == 0) // ftp://<user>:<password>@<host>/<url-path>
+    if (regexec(&url_username_password, url, 0, NULL, 0) == 0) // ftp://<user>:<password>@<host>/<url-path>
     {
         sscanf(url, "%*[^/]//%[^:]", information->user);
         sscanf(url, "%*[^/]//%*[^:]%[^@]", information->password);
         sscanf(url, "%*[^@]%[^/]", information->host);
         sscanf(url, "%*[^@]%*[^/]%s", information->urlPath);
     }
-    else if (regexec(&urlUsername, url, 0, NULL, 0) == 0) // ftp://<user>@<host>/<url-path>
+    else if (regexec(&url_username, url, 0, NULL, 0) == 0) // ftp://<user>@<host>/<url-path>
     {
         sscanf(url, "%*[^/]//%[^@]", information->user);
         sscanf(url, "%*[^@]%[^/]", information->host);
         sscanf(url, "%*[^@]%*[^/]%s", information->urlPath);
 
-        information->password = DEFAULT_PASSWORD;
+        strcpy(information->password, DEFAULT_PASSWORD);
     }
-    else if (regexec(&urlNoUser, url, 0, NULL, 0) == 0) // ftp://<host>/<url-path>
+    else if (regexec(&url_no_user, url, 0, NULL, 0) == 0) // ftp://<host>/<url-path>
     {
-        sscanf(url, "%*[^:]//%[^/]", information->host);
-        sscanf(url, "%*[^:]//%*[^/]%s", information->urlPath);
+        sscanf(url, "%*[^/]//%[^/]", information->host);
+        sscanf(url, "%*[^/]//%*[^/]%s", information->urlPath);
 
-        information->user = DEFAULT_USER;
-        information->password = DEFAULT_PASSWORD;
+        strcpy(information->user, DEFAULT_USER);
+        strcpy(information->password, DEFAULT_PASSWORD);
     }
     else
     {
@@ -185,9 +197,9 @@ void parseURl(char *url, struct urlInfo *information)
 
     if (regexec(&ip, information->host, 0, NULL, 0) == REG_NOMATCH) // Host is not an IP
     {
-        char *ip;
+        char ip[16];
 
-        if (getIP(information->host, &ip) == 0)
+        if (get_IP(information->host, ip) == 0)
         {
             strcpy(information->host, ip);
         }
@@ -227,29 +239,274 @@ void parseURl(char *url, struct urlInfo *information)
     */
 }
 
-int main(int argc, char **argv)
+int send_command(char *instruction, struct server_response *response)
 {
+    char command[6];
 
-    if (argc > 1)
-        printf("**** No arguments needed. They will be ignored. Carrying ON.\n");
-
-    char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-    size_t bytes;
-
-    /*send a string to the server*/
-    bytes = write(sockfd, buf, strlen(buf));
-    if (bytes > 0)
-        printf("Bytes escritos %ld\n", bytes);
+    sprintf(command, "%s\n", instruction);
+    if (write(control_socket, command, strlen(command)) == -1)
+    {
+        perror("write() server command\n");
+        exit(-1);
+    }
     else
     {
-        perror("write()");
+        unsigned char state = ST_WAIT;
+
+        while (state == ST_WAIT)
+        {
+            get_server_response(response);
+            unsigned char response_type = response->code / 100;
+
+            switch (state)
+            {
+            case ST_WAIT:
+                switch (response_type)
+                {
+                case 1:
+                case 3:
+                    state = ST_ERROR;
+                    break;
+
+                case 2:
+                    state = ST_SUCCESS;
+                    break;
+                case 4:
+                case 5:
+                    state = ST_FAILURE;
+                    break;
+                }
+                break;
+            case ST_ERROR:
+                printf("Error in command %s: %s", instruction, response->message);
+                exit(-1);
+                break;
+            case ST_SUCCESS:
+                // printf("Success: %s", response->message);
+                break;
+            case ST_FAILURE:
+                printf("Failure in command %s: %s", instruction, response->message);
+                exit(-1);
+                break;
+            }
+        }
+        return response->code;
+    }
+}
+
+int request_file(char *resource, struct server_response *response)
+{
+
+    char command[strlen(resource) + 7];
+
+    sprintf(command, "RETR %s\n", resource);
+    if (write(control_socket, command, strlen(command)) == -1)
+    {
+        perror("write() server command\n");
         exit(-1);
     }
 
-    if (close(sockfd) < 0)
+    unsigned char state = ST_WAIT;
+
+    while (state == ST_WAIT)
+    {
+        get_server_response(response);
+        unsigned char response_type = response->code / 100;
+
+        switch (state)
+        {
+        case ST_WAIT:
+            switch (response_type)
+            {
+            case 1:
+                break;
+            case 2:
+                state = ST_SUCCESS;
+                break;
+            case 3:
+                state = ST_ERROR;
+                break;
+            case 4:
+            case 5:
+                state = ST_FAILURE;
+                break;
+            }
+            break;
+        case ST_ERROR:
+            printf("Error in command RETR: %s", response->message);
+            exit(-1);
+            break;
+        case ST_SUCCESS:
+            // printf("Success: %s", response->message);
+            break;
+        case ST_FAILURE:
+            printf("Failure in command RETR: %s", response->message);
+            exit(-1);
+            break;
+        }
+    }
+    return response->code;
+}
+
+int login(char *user, char *pass)
+{
+    char command[max(strlen(user), strlen(pass)) + 7];
+    sprintf(command, "USER %s\n", user);
+    if (write(control_socket, command, strlen(command)) == -1)
+    {
+        perror("write() server command");
+        exit(-1);
+    }
+
+    unsigned char state = ST_WAIT_USER;
+    struct server_response response;
+
+    while (state != ST_SUCCESS)
+    {
+        get_server_response(&response);
+        unsigned char response_type = response.code / 100;
+
+        switch (state)
+        {
+        case ST_WAIT_USER:
+
+            switch (response_type)
+            {
+            case 1:
+                state = ST_ERROR;
+                break;
+            case 2:
+                state = ST_SUCCESS;
+                break;
+            case 4:
+            case 5:
+                state = ST_FAILURE;
+                break;
+            case 3:
+                sprintf(command, "PASS %s\n", pass);
+                if (write(control_socket, command, strlen(command)) == -1)
+                {
+                    perror("write() server command");
+                    exit(-1);
+                }
+                state = ST_WAIT_PASS;
+                break;
+            }
+            break;
+        case ST_WAIT_PASS:
+            switch (response_type)
+            {
+            case 1:
+                state = ST_ERROR;
+                break;
+            case 2:
+                state = ST_SUCCESS;
+                break;
+            case 3:
+                char input[100];
+                printf("The server is requesting your account name: ");
+                scanf("%s", input);
+
+                sprintf(command, "ACCT %s\n", pass);
+                if (write(control_socket, command, strlen(command)) == -1)
+                {
+                    perror("write() server command");
+                    exit(-1);
+                }
+                state = ST_WAIT_ACCT;
+                break;
+            case 4:
+            case 5:
+                state = ST_FAILURE;
+                break;
+            }
+            break;
+        case ST_WAIT_ACCT:
+            switch (response_type)
+            {
+            case 1:
+            case 3:
+                state = ST_ERROR;
+                break;
+            case 2:
+                state = ST_SUCCESS;
+                break;
+            case 4:
+            case 5:
+                state = ST_FAILURE;
+                break;
+            }
+            break;
+        case ST_ERROR:
+            printf("Error Login: %s\n", response.message);
+            exit(-1);
+            break;
+        case ST_SUCCESS:
+            // printf("Success: %s", response->message);
+            break;
+        case ST_FAILURE:
+            printf("Failure Login: %s\n", response.message);
+            exit(-1);
+            break;
+        }
+    }
+    return response.code;
+}
+
+void get_file(char *filename)
+{
+
+    FILE *file = fopen(filename, "wb");
+    if (file == NULL)
+    {
+        printf("Error: Opening file %s\n", filename);
+        exit(-1);
+    }
+
+    char file_content[MAX_FILE_LENGTH];
+
+    unsigned short size;
+    while (size = read(data_socket, file_content, MAX_FILE_LENGTH))
+    {
+        if (fwrite(file_content, size, 1, file) < 0)
+        {
+            printf("Error: Writing to file %s\n", filename);
+            exit(-1);
+        }
+    }
+    fclose(file);
+}
+
+void close_connect()
+{
+    struct server_response temp;
+    send_command("QUIT", &temp);
+    if (close(control_socket) < 0 || close(data_socket) < 0)
     {
         perror("close()");
         exit(-1);
     }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Wrong format. Usage: ./download  ftp://[<user>:<password>@]<host>/<url-path>\n");
+        exit(-1);
+    }
+
+    struct urlInfo arguments;
+    parse_URl(argv[1], &arguments);
+
+    open_socket(arguments.host, DEFAULT_PORT, control_socket);
+    login(arguments.user, arguments.password);
+    /*     struct server_response response;
+        send_command("PASV", &response);
+        open_socket(response.server_ip, response.server_port, data_socket);
+        request_file(arguments.urlPath, &response);
+        get_file(arguments.urlPath);
+        close_connect(); */
+
     return 0;
 }
--- part1/download.h
+++ part1/download.h
@@ -11,29 +11,38 @@
 #include <regex.h>
 #include <netdb.h>
 
-
-struct serverResponse
+struct server_response
 {
     unsigned short int code;
-    char *server_ip;
-    unsigned short int server_port;
+    char server_ip[16];
+    unsigned int server_port;
+    char message[500];
 };
 
 struct urlInfo
 {
-    char *user;
-    char *password;
-    char *host;
-    char *urlPath;
+    char user[100];
+    char password[100] ;
+    char host[100];
+    char urlPath[100];
 };
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 
 #define SERVER_PORT 6000
 #define SERVER_ADDR "192.168.28.96"
 
+#define MAX_FILE_LENGTH 500
+
+#define DEFAULT_PORT 21
+
 // Server Response Codes
 #define SERVER_ENTER_PSV 227
 
-// State machine states
+// State machine for parsing server responses
 #define ST_READING_CODE 0
 #define ST_PARSE_START 1
 #define ST_PARSE 2
@@ -41,11 +50,22 @@ struct urlInfo
 #define ST_FLUSH_SINGLE 4
 #define ST_END 5
 
+//State machine for RETR
+#define ST_WAIT 0
+#define ST_ERROR 1
+#define ST_SUCCESS 2
+#define ST_FAILURE 3
+
+//State machine for login
+#define ST_WAIT_USER 4
+#define ST_WAIT_PASS 5
+#define ST_WAIT_ACCT 6
+
 // Regexs for parsing URL
-#define REGEX_URL_USERNAME_PASSWORD "^ftp:\/\/.*:.*@.*\/.*$"
-#define REGEX_URL_USERNAME "^ftp:\/\/[^:]*@.*\/.*$"
-#define REGEX_URL_NO_USER "^ftp:\/\/[^@:]*\/.*$"
-#define REGEX_IP "^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
+#define REGEX_URL_USERNAME_PASSWORD "^ftp://.*:.*@.*/.*$"
+#define REGEX_URL_USERNAME "^ftp://[^:]*@.*/.*$"
+#define REGEX_URL_NO_USER "^ftp://[^@:]*/.*$"
+#define REGEX_IP "^((25[0-5]|(2[0-4]|1[:digit:]|[1-9]|)[:digit:])\\.?\b){4}$"
 
 // Default authentication parameters
 #define DEFAULT_USER "anonymous"
